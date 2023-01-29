0ce9e4e4837bcce45cab2fe64de319cfb96db55e
Mac Sweeny <macsweenydev@gmail.com>
2023-01-29 15:01:18 +1000

    fix: removed unused pages

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -4,11 +4,8 @@ import Link from 'next/link'
 import clsx from 'clsx'
 
 import { Container } from '@/components/Container'
-import {
-  GitHubIcon,
-  LinkedInIcon,
-} from '@/components/SocialIcons'
-import portraitImage from '@/images/portrait.jpg'
+import { GitHubIcon, LinkedInIcon } from '@/components/SocialIcons'
+import avatarImage from '@/images/avatar.jpg'
 
 function SocialLink({ className, href, children, icon: Icon }) {
   return (
@@ -39,10 +36,10 @@ export default function About() {
   return (
     <>
       <Head>
-        <title>About - Spencer Sharp</title>
+        <title>About - Mac Sweeny</title>
         <meta
           name="description"
-          content="I’m Spencer Sharp. I live in New York City, where I design the future."
+          content="I’m Mac Sweeny. I live in Brisbane, Australia."
         />
       </Head>
       <Container className="mt-16 sm:mt-32">
@@ -50,7 +47,7 @@ export default function About() {
           <div className="lg:pl-20">
             <div className="max-w-xs px-2.5 lg:max-w-none">
               <Image
-                src={portraitImage}
+                src={avatarImage}
                 alt=""
                 sizes="(min-width: 1024px) 32rem, 20rem"
                 className="aspect-square rotate-3 rounded-2xl bg-zinc-100 object-cover dark:bg-zinc-800"
@@ -59,52 +56,63 @@ export default function About() {
           </div>
           <div className="lg:order-first lg:row-span-2">
             <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-              I’m Spencer Sharp. I live in New York City, where I design the
-              future.
+              I’m Mac Sweeny. I live in Brisbane, Australia.
             </h1>
             <div className="mt-6 space-y-7 text-base text-zinc-600 dark:text-zinc-400">
               <p>
-                I’ve loved making things for as long as I can remember, and
-                wrote my first program when I was 6 years old, just two weeks
-                after my mom brought home the brand new Macintosh LC 550 that I
-                taught myself to type on.
+                I’m a passionate, hardworking, young guy who loves to learn and
+                challenge myself with new technologies and programming
+                languages. I am always eager to take on new projects and tasks,
+                and enjoy working in a fast-paced, dynamic environment. I am a
+                quick learner and am able to adapt to new situations easily. I
+                am also a strong team player and enjoy collaborating with others
+                to solve complex problems and deliver high-quality results.
               </p>
               <p>
-                The only thing I loved more than computers as a kid was space.
-                When I was 8, I climbed the 40-foot oak tree at the back of our
-                yard while wearing my older sister’s motorcycle helmet, counted
-                down from three, and jumped — hoping the tree was tall enough
-                that with just a bit of momentum I’d be able to get to orbit.
+                Currently, I am focusing on learning and developing the various
+                components that make up a full-stack web application, with a
+                particular emphasis on Software as a Service (SaaS) products.
+                This encompasses gaining knowledge about different front-end
+                stacks, such as TailwindCSS and React, as well as back-end
+                technologies, Prisma, tRPC, and Next.js. Furthermore, I am
+                highly motivated to learn about the different methodologies and
+                best practices associated with building, deploying, and
+                maintaining SaaS products. To accomplish this, I am actively
+                working on a SaaS product called Onu, which enables users to
+                create and manage their own social media communities.
               </p>
               <p>
-                I spent the next few summers indoors working on a rocket design,
-                while I recovered from the multiple surgeries it took to fix my
-                badly broken legs. It took nine iterations, but when I was 15 I
-                sent my dad’s Blackberry into orbit and was able to transmit a
-                photo back down to our family computer from space.
-              </p>
-              <p>
-                Today, I’m the founder of Planetaria, where we’re working on
-                civilian space suits and manned shuttle kits you can assemble at
-                home so that the next generation of kids really <em>can</em>{' '}
-                make it to orbit — from the comfort of their own backyards.
+                Another one of my goals is to secure a full-time position as a
+                Software Engineer within a corporate environment. I am currently
+                seeking such a position in Brisbane, Australia, but I am open to
+                remote opportunities as well. I am particularly interested in
+                working with a team of like-minded individuals so that I can
+                continue to develop my skills as a developer.
               </p>
             </div>
           </div>
           <div className="lg:pl-20">
             <ul role="list">
-              <SocialLink href="https://github.com/mac-eth" icon={GitHubIcon} className="mt-4">
+              <SocialLink
+                href="https://github.com/mac-eth"
+                icon={GitHubIcon}
+                className="mt-4"
+              >
                 Follow on GitHub
               </SocialLink>
-              <SocialLink href="https://www.linkedin.com/in/mac-sweeny-a2b492219/" icon={LinkedInIcon} className="mt-4">
+              <SocialLink
+                href="https://www.linkedin.com/in/mac-sweeny-a2b492219/"
+                icon={LinkedInIcon}
+                className="mt-4"
+              >
                 Follow on LinkedIn
               </SocialLink>
               <SocialLink
-                href="mailto:spencer@planetaria.tech"
+                href="mailto:macsweenydev@gmail.com"
                 icon={MailIcon}
                 className="mt-8 border-t border-zinc-100 pt-8 dark:border-zinc-700/40"
               >
-                spencer@planetaria.tech
+                macsweenydev@gmail.com
               </SocialLink>
             </ul>
           </div>
--- src/pages/articles/index.jsx
+++ src/pages/articles/index.jsx
@@ -38,15 +38,15 @@ export default function ArticlesIndex({ articles }) {
   return (
     <>
       <Head>
-        <title>Articles - Spencer Sharp</title>
+        <title>Articles - Mac Sweeny</title>
         <meta
           name="description"
-          content="All of my long-form thoughts on programming, leadership, product design, and more, collected in chronological order."
+          content="A few of my thoughts and ideas surrounding programming and product ideas, collected in chronological order."
         />
       </Head>
       <SimpleLayout
-        title="Writing on software design, company building, and the aerospace industry."
-        intro="All of my long-form thoughts on programming, leadership, product design, and more, collected in chronological order."
+        title="Just some Thoughts and Ideas"
+        intro="A few of my thoughts and ideas surrounding programming and product ideas, collected in chronological order."
       >
         <div className="md:border-l md:border-zinc-100 md:pl-6 md:dark:border-zinc-700/40">
           <div className="flex max-w-3xl flex-col space-y-16">
